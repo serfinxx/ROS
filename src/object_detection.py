fe95e3b7d2e25f1320d29cbac95588dc4c32935c
Xuan-Rui Fan <lhsu1@sheffield.ac.uk>
2021-05-03 20:55:46 +0100

    object detection updated

--- src/object_detection.py
+++ src/object_detection.py
@@ -14,6 +14,11 @@ from sensor_msgs.msg import Image
 from move_tb3 import MoveTB3
 from tb3_odometry import TB3Odometry
 
+# Import other modules
+import numpy as np
+import math
+import time
+
 class object_detection(object):
 
     def __init__(self):
@@ -26,26 +31,27 @@ class object_detection(object):
         self.robot_controller = MoveTB3()
         self.robot_odom = TB3Odometry()
         
-        # Default speed
+        # Default vars
         self.robot_controller.set_move_cmd(0.0, 0.0)
-        self.status = 1
-        
-
-        self.move_rate = '' # fast, slow or stop
-        self.stop_counter = 0
+        self.hsv_img = np.zeros((1920,1080,3), np.uint8)
+        self.target_color_bounds = ([0, 255, 100], [0, 255, 255])
+        self.status = 0
 
         # Shutdown hook 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdown_ops)
 
         self.rate = rospy.Rate(5)
-        
-        self.m00 = 0
-        self.m00_min = 100000
 
         # Blue, Red, Green, Turquoise, Purple, Yellow hsv colour values:
-        self.lower = [(115, 224, 100), (0, 185, 100), (25, 150, 100), (75, 150, 100), (145, 185, 100), (28, 180, 100)]
-        self.upper = [(130, 255, 255), (10, 255, 255), (70, 255, 255), (100, 255, 255), (150, 250, 255), (32, 255, 255)]
+        self.colour_boundaries = {
+            "Red":    ([0, 185, 100], [10, 255, 255]),
+            "Blue":   ([115, 224, 100],   [130, 255, 255]),
+            "Yellow": ([28, 180, 100], [32, 255, 255]),
+            "Green":   ([25, 150, 100], [70, 255, 255]),
+            "Turquoise":   ([75, 150, 100], [100, 255, 255]),
+            "Purple":   ([145, 185, 100], [150, 250, 255])
+        }
 
     def shutdown_ops(self):
         self.robot_controller.stop()
@@ -65,56 +71,50 @@ class object_detection(object):
         crop_y = int((height/2) - (crop_height/2))
 
         crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
-        hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        self.hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
-        colours = ['Blue','Red','Green','Turquoise','Purple','Yellow']
-
-        for i in range(4):
-            if i == 0:
-                mask = cv2.inRange(hsv_img, self.lower[i], self.upper[i])
-            else:
-                mask = mask + cv2.inRange(hsv_img, self.lower[i], self.upper[i])
-
-        m = cv2.moments(mask)
-            
-        self.m00 = m['m00']
-        self.cy = m['m10'] / (m['m00'] + 1e-5)
-
-        if self.m00 > self.m00_min:
-            cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
-        
         cv2.imshow('cropped image', crop_img)
         cv2.waitKey(1)
 
-    def rotate_180_degree(self, init_yaw=0.0):
-        init_yaw = (init_yaw+360)%360
-        while abs(((init_yaw+180)+360)%360-((self.robot_odom.yaw+360)%360)) > 1:
-            self.robot_controller.set_move_cmd(0.0, 0.2)
-            self.robot_controller.publish()
-            print(self.robot_odom.yaw)
-        
+    def rotate_by_degree(self, degree, direction="left"):
+        time.sleep(1)
+        speed = 0.2
+        t = math.radians(degree) / speed
+        if direction is "right":
+            self.robot_controller.set_move_cmd(0.0, -speed)
+        else:
+            self.robot_controller.set_move_cmd(0.0, speed)
+        self.robot_controller.publish()
+        time.sleep(t)
+
         self.robot_controller.stop()
 
+    def colour_selection(self):
+        for colour_name, (lower, upper) in self.colour_boundaries.items():
+            lower_bound = np.array(lower)
+            upper_bound = np.array(upper)
+            mask = cv2.inRange(self.hsv_img, lower_bound, upper_bound)
+            if mask.any():
+                print("SEARCH INITIATED: The target colour is {}".format (colour_name))
+                self.target_color_bounds = self.colour_boundaries[colour_name]
+                break
         
-
-
     def main(self):
         while not self.ctrl_c:
             # Strats here:
-            if self.status == 1:
-                # get current degree
-                current_yaw = self.robot_odom.yaw
+            if self.status == 0:
                 # rotate 180 degrees
-                self.rotate_180_degree(init_yaw=current_yaw)
+                self.rotate_by_degree(90)
                 self.status += 1
+            if self.status == 1:
+                self.colour_selection()
+                self.status += 1
+            if self.status == 2:
+                self.rotate_by_degree(90, "right")
+                self.status +=1
             else:
-                self.robot_controller.stop()
+                break
             
-            
-
-
-
-
             
             self.robot_controller.publish()
             self.rate.sleep()
