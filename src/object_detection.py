be0cb069b9ded84ec2b3009a8bcda595bc613313
Xuan-Rui Fan <lhsu1@sheffield.ac.uk>
2021-05-03 13:07:51 +0100

    object detection updated

---
+++ src/object_detection.py
@@ -0,0 +1,101 @@
+#! /usr/bin/python
+
+# Import the core Python modules for ROS and to implement ROS Actions:
+import rospy
+
+# Import some image processing modules:
+import cv2
+from cv_bridge import CvBridge
+
+# Import all the necessary ROS message types:
+from sensor_msgs.msg import Image
+
+# Import some other modules from within this package
+from move_tb3 import MoveTB3
+
+class object_detection(object):
+
+    def __init__(self):
+        rospy.init_node('object_detection')
+        self.base_image_path = '/home/student/myrosdata/week6_images'
+        self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw",
+            Image, self.camera_callback)
+        self.cvbridge_interface = CvBridge()
+
+        self.robot_controller = MoveTB3()
+        
+        # Default speed
+        self.robot_controller.set_move_cmd(0.0, 0.0)
+
+        self.move_rate = '' # fast, slow or stop
+        self.stop_counter = 0
+
+        # Shutdown hook 
+        self.ctrl_c = False
+        rospy.on_shutdown(self.shutdown_ops)
+
+        self.rate = rospy.Rate(5)
+        
+        self.m00 = 0
+        self.m00_min = 100000
+
+        # Blue, Red, Green, Turquoise, Purple, Yellow hsv colour values:
+        self.lower = [(115, 224, 100), (0, 185, 100), (25, 150, 100), (75, 150, 100), (145, 185, 100), (28, 180, 100)]
+        self.upper = [(130, 255, 255), (10, 255, 255), (70, 255, 255), (100, 255, 255), (150, 250, 255), (32, 255, 255)]
+
+    def shutdown_ops(self):
+        self.robot_controller.stop()
+        cv2.destroyAllWindows()
+        self.ctrl_c = True
+    
+    def camera_callback(self, img_data):
+        try:
+            cv_img = self.cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        
+        height, width, channels = cv_img.shape
+        crop_width = width - 800
+        crop_height = 400
+        crop_x = int((width/2) - (crop_width/2))
+        crop_y = int((height/2) - (crop_height/2))
+
+        crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
+        hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+
+        colours = ['Blue','Red','Green','Turquoise','Purple','Yellow']
+
+        for i in range(4):
+            if i == 0:
+                mask = cv2.inRange(hsv_img, self.lower[i], self.upper[i])
+            else:
+                mask = mask + cv2.inRange(hsv_img, self.lower[i], self.upper[i])
+
+        m = cv2.moments(mask)
+            
+        self.m00 = m['m00']
+        self.cy = m['m10'] / (m['m00'] + 1e-5)
+
+        if self.m00 > self.m00_min:
+            cv2.circle(crop_img, (int(self.cy), 200), 10, (0, 0, 255), 2)
+        
+        cv2.imshow('cropped image', crop_img)
+        cv2.waitKey(1)
+
+    def main(self):
+        while not self.ctrl_c:
+            # Strats here:
+
+
+
+
+            
+            self.robot_controller.publish()
+            self.rate.sleep()
+            
+if __name__ == '__main__':
+    object_detection = object_detection()
+    try:
+        object_detection.main()
+    except rospy.ROSInterruptException:
+        pass
