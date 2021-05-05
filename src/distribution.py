84980c65fd705eb88a13dbffbbf684d94d3a97c9
Xuan-Rui Fan <lhsu1@sheffield.ac.uk>
2021-05-05 18:00:22 +0100

    distribution added

---
+++ src/distribution.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+class Distribution(object):
+    def __init__(self):
+        self.x = random.randrange(0,10000) 
+
+    def levy(self, scale=1):
+        pd = (scale/math.sqrt(2*math.pi*math.pow(self.x,3)))*math.exp(-scale/(2*self.x))*math.pow(self.x,-3/2)
+        return pd
+
+    def uniform(self, start=0, stop=9):
+        return random.randint(start, stop)
